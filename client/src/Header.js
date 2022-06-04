4bc5b5a93694bc3f0a8325f57cb15426e5fc34fb
Anubrata007 <anubrata995@gmail.com>
2022-06-05 00:45:02 +0530

    First commit

---
+++ client/src/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Header.css';
+import PersonIcon from '@material-ui/icons/Person';
+import ForumIcon from '@material-ui/icons/Forum';
+import { IconButton } from '@mui/material';
+
+function Header() {
+  return (
+    <div className='header'>
+      <IconButton>
+        <PersonIcon fontSize='large' className='header__icon' />
+      </IconButton>
+
+      <img className='header__logo' src='https://brandlogos.net/wp-content/uploads/2021/09/tinder-flame-logo.png' alt='logo' />
+
+      <IconButton>
+        <ForumIcon fontSize='large' className='header__icon' />
+      </IconButton>
+
+    </div>
+  )
+}
+
+export default Header;
